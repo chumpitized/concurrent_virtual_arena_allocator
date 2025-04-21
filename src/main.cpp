5c27e00562ed3a3d190f8500cbd991b5eac9c588
chumpitized <126842502+chumpitized@users.noreply.github.com>
2025-04-21 10:32:03 -0500

    update to use VirtualAlloc for reservation and commitment of memory

--- src/main.cpp
+++ src/main.cpp
@@ -19,11 +19,12 @@ typedef struct Arena Arena;
 struct Arena {
 	unsigned char *buffer;
 	size_t length;
+	std::atomic<size_t> committed;
 	std::atomic<size_t> curr_offset;
 };
 
-void arena_init(Arena *a, void *buffer, size_t length) {
-	a->buffer = (unsigned char *)buffer;
+void arena_init(Arena *a, size_t length) {
+	a->buffer = (unsigned char *)VirtualAlloc(NULL, length, MEM_RESERVE, PAGE_READWRITE);
 	a->length = length;
 	a->curr_offset.store(0);
 }
@@ -36,13 +37,32 @@ inline size_t align_forward(size_t size, size_t align) {
 	return (size + align - 1) & ~(align - 1);
 }
 
+inline size_t commit_size(size_t committed, size_t allocated) {
+	size_t diff = allocated - committed;
+	return (diff + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1);
+}
+
 void *arena_concurrent_alloc(Arena *a, size_t size, size_t align) {
 	size_t aligned_size = align_forward(size, align);
 	size_t offset = a->curr_offset.fetch_add(aligned_size);
+	size_t allocated = offset + size;
 	
-	if (offset + size > a->length) {
+	if (allocated > a->length) {
 		a->curr_offset.fetch_sub(aligned_size);
 		return NULL;
+	}
+
+	size_t committed = a->committed.load();
+
+	while (true) {
+		if (allocated <= committed) break;
+		size_t to_commit = commit_size(committed, allocated);
+
+		VirtualAlloc(&a->buffer[committed], to_commit, MEM_COMMIT, PAGE_READWRITE);
+
+		if (a->committed.compare_exchange_weak(committed, to_commit)) {
+			break;
+		}
 	}
 
 	return &a->buffer[offset];
@@ -55,16 +75,17 @@ void arena_clear(Arena *a) {
 void allocate(Arena *a) {
 	for (int i = 0; i < 1024; ++i) {
 		std::cout << "Allocating int from thread ID: " << std::this_thread::get_id() << std::endl;
-		arena_concurrent_alloc(a, 4, DEFAULT_ALIGNMENT);
+		void *ptr = arena_concurrent_alloc(a, 4, DEFAULT_ALIGNMENT);
+		//To make sure the memory is actually committed and not just reserved, we allocate an int.
+		*(int *)ptr = 4;
 		std::cout << "Current offset is " << a->curr_offset.load() << std::endl;
 	}
 }
 
 int main() {
 	size_t mb = 1024 * 1024;
-	void *buffer = malloc(mb);
 	Arena *a = new Arena;
-	arena_init(a, buffer, mb);
+	arena_init(a, mb);
 
 	std::thread t1(allocate, a);
 	std::thread t2(allocate, a);
